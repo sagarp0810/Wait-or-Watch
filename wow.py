6cf83bebb305c21cee38f0f8c2638992e04b4a37
Sagar Pamnani <sagarpamnani77@gmail.com>
2018-10-11 07:12:25 +0530

    minor changes

--- wow.py
+++ wow.py
@@ -14,7 +14,7 @@ def isInternetOn():
     try:
         urllib.urlopen('https://www.google.com/', timeout=1)
         return True
-    except urllib.URLError as err: 
+    except: 
         return False
 
 def monthToNum(month):
@@ -53,7 +53,7 @@ def dateToNum(s):
 	if len(s)==0:
 		date = 0
 	elif len(s)==4:
-		date =  int(s)*10000 + 1232
+		date =  int(s)*10000 + 1232				# for year, date is yyyy1232
 	else:
 		date = int(s[-4:len(s)])*10000
 		date += monthToNum(s[-9:-6])*100
@@ -76,136 +76,168 @@ def showStatus(d):
 	else:
 		status = "The next episode airs on " + numToDate(str(d)) + "."
 	return status
+	
+def insertIntoDB(email, tv_series):
+	try:
+		print "\nAdding to DB..."
+		mydb = mysql.connector.connect(host="localhost", user="root", passwd="qwerty")
+		cursor = mydb.cursor()
+		cursor.execute("CREATE DATABASE IF NOT EXISTS sde")
 
+		mydb = mysql.connector.connect(host="localhost", user="root", passwd="qwerty", db="sde")
+		cursor = mydb.cursor()
+		cursor.execute("""CREATE TABLE IF NOT EXISTS waitorwatch (
+						id INT PRIMARY KEY AUTO_INCREMENT,
+						email VARCHAR(255) NOT NULL,
+						tv_series VARCHAR(255) NOT NULL)""")
+		
+		sql = "INSERT INTO waitorwatch (email, tv_series) VALUES (%s, %s)"
+		val = (email, tv_series)
+		cursor.execute(sql, val)
+		mydb.commit()
+		mydb.close()
+		print "Added to DB successfully!\n"
+		
+	except:
+		print "Unable to add input to DB!\n"
+	
+def getStatus(tv_series):
+	print "Fetching Data..."
+	tv_series = [s.strip() for s in tv_series.split(',')]
+	x = datetime.date.today()
+	cur_date = x.year*10000 + x.month*100 + x.day				# current date in yyyymmdd format
+	data = ''
+	body = ''
+	
+	for series in tv_series:
+		date = []
+		temp = 0
+
+		try:
+			wiki = "https://www.imdb.com/find?q=" + series				# search tv series on imdb
+			page = requests.get(wiki).text
+			soup = BeautifulSoup(page, 'lxml')
+
+			search = soup.find('div', class_='findSection').table.find('tr').find('td').a		# anchor tag of first search item
+			href = search['href']
+			tv_id = href.split('/')[2]
+
+			wiki = "https://www.imdb.com/title/" + tv_id				# imdb page of the tv series
+			page = requests.get(wiki).text
+			soup = BeautifulSoup(page, 'lxml')
+
+			season_div = soup.find('div', class_='seasons-and-year-nav')
+			season = season_div.find_all('div')[2].find('a').text				# latest season number
+			
+			rating_div = soup.find('div', class_='ratingValue')
+			rating = rating_div.strong.span.text
+
+			title_div = soup.find('div', class_='title_wrapper')
+			series = title_div.h1.text.strip()
+			
+			poster_div = soup.find('div', class_='poster')
+			poster = poster_div.a.img
+			link = poster['src']								# link for the poster
+			filename = series + ".jpg"
+			
+			wiki = "https://www.imdb.com/title/" + tv_id + "/episodes?season=" + season
+			page = requests.get(wiki).text
+			soup = BeautifulSoup(page, 'lxml')
+			
+		except:
+			print "No TV Series found with name " + series + "."
+			continue				# input tv series which are not found will not be included in email
+		
+		for i in soup.find_all('div', class_='airdate'):
+			date.append(dateToNum(i.text.strip()))				# list having dates of all episodes in yyyymmdd format
+			
+		for d in date:
+			if date[0]==0:
+				temp = 1
+				break
+			if d>cur_date:
+				temp = d
+				break
+			if d==0:
+				temp = 2
+				break
+		
+		status = showStatus(temp)	
+		data += "TV Series name: " + series + "\n"
+		data += "IMDb Rating: " + rating + "\n"
+		data += "Status: " + status + "\n\n"
+		
+		body += "<div style='width:40%; height:200px; float:left;'>"
+		body += "<a href='https://www.imdb.com/title/" + tv_id + "'>"
+		body += "<img alt='" + series + " Poster' src='" + link + "' height=192px></a></div>"
+		body += "<div style='width:60%; height:200px; float:left;'><br><br>"
+		body += "<b>TV Series name: </b>" + series + "<br><br>"
+		body += "<b>IMDb Rating: </b>" + rating + "<br><br>"
+		body += "<b>Status: </b>" + status + "</div><hr>"	
+
+	if body=='':				# if input tv series are not found, then body is empty
+		exit()
+	else:
+		data = data[0:-2]
+		body = body[0:-4]
+	
+	print "\nFetched Data:"
+	print data
+	return body
+	
+def sendEmail(email, body):
+	print "\nSending email..."
+	SMTPserver = 'smtp.gmail.com'
+	sender = 'waitorwatch@gmail.com'
+	password = "W@it0rW@tch"
+	subject = "TV Series Information"
+
+	try:
+		msg = MIMEMultipart()
+		msg['From'] = sender
+		msg['To'] = email
+		msg['Subject'] = subject
+		msg.attach(MIMEText(body, 'html'))				# attach the body as html code
+
+		conn = SMTP_SSL(SMTPserver)
+		conn.set_debuglevel(False)
+		conn.login(sender, password)
+
+		try:
+			conn.sendmail(sender, [email], msg.as_string())
+			print "Email sent to " + email + " successfully!"
+		finally:
+			conn.quit()
+
+	except:
+		print "Unable to send Email!"
+
+# to check if user has an active internet connection
 if not isInternetOn:
 	print "Uh Oh! Seems that you are not connected to the Internet. Please check and try again later."
 	exit()
 
-mydb = mysql.connector.connect(
-	host = "localhost",
-	user = "root",
-	passwd = "qwerty"
-)
-
-cursor = mydb.cursor()
-cursor.execute("CREATE DATABASE IF NOT EXISTS sde")
-
-mydb = mysql.connector.connect(
-	host = "localhost",
-	user = "root",
-	passwd = "qwerty",
-	database = "sde"
-)
-
-cursor = mydb.cursor()
-cursor.execute("CREATE TABLE IF NOT EXISTS waitorwatch (email VARCHAR(255), tv_series VARCHAR(255))")
-
-x = datetime.date.today()
-cur_date = x.year*10000 + x.month*100 + x.day
-
 print "Welcome to Wait-or-Watch!!!\n"
-print "Email address:",
-email = raw_input()
-print "TV Series:",
-tv_series = raw_input()
-print
 
-sql = "INSERT INTO waitorwatch VALUES (%s, %s)"
-val = (email, tv_series)
-cursor.execute(sql, val)
-mydb.commit()
+while True:
+	print "Email address:",
+	email = raw_input()
+	match = re.match('^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$', email)
 
-SMTPserver = 'smtp.gmail.com'
-sender = 'waitorwatch@gmail.com'
-destination = email
-password = "W@it0rW@tch"
-subject = "TV Series Information"
-body = ''
+	if(match == None):								# check proper email format
+		print("Invalid Email, Try Again.\n")
+	else:
+		break
+    
+while True:
+	print "TV Series:",
+	tv_series = raw_input().strip()
 
-tv_series = [s.strip() for s in tv_series.split(',')]
+	if(tv_series==''):
+		print("Invalid input, Try Again.\n")
+	else:
+		break
 
-for series in tv_series:
-	date = []
-	temp = 0
-
-	try:
-		wiki = "https://www.imdb.com/find?q=" + series
-		page = requests.get(wiki).text
-		soup = BeautifulSoup(page, 'lxml')
-
-		search = soup.find('div', class_='findSection').table.find('tr').find('td').a
-		href = search['href']
-		tv_id = href.split('/')[2]
-
-		wiki = "https://www.imdb.com/title/" + tv_id
-		page = requests.get(wiki).text
-		soup = BeautifulSoup(page, 'lxml')
-
-		season_div = soup.find('div', class_='seasons-and-year-nav')
-		season = season_div.find_all('div')[2].find('a').text
-		
-		rating_div = soup.find('div', class_='ratingValue')
-		rating = rating_div.strong.span.text
-
-		title_div = soup.find('div', class_='title_wrapper')
-		series = title_div.h1.text.strip()
-		
-		poster_div = soup.find('div', class_='poster')
-		poster = poster_div.a.img
-		link = poster['src']
-		filename = series + ".jpg"
-		
-		wiki = "https://www.imdb.com/title/" + tv_id + "/episodes?season=" + season
-		page = requests.get(wiki).text
-		soup = BeautifulSoup(page, 'lxml')
-		
-	except:
-		print "No TV Series found with name " + series + "."
-		continue
-	
-	for i in soup.find_all('div', class_='airdate'):
-		date.append(dateToNum(i.text.strip()))
-		
-	for d in date:
-		if date[0]==0:
-			temp = 1
-			break
-		if d>cur_date:
-			temp = d
-			break
-		if d==0:
-			temp = 2
-			break
-			
-	body += "<div style='width:40%; height:200px; float:left;'>"
-	body += "<a href='https://www.imdb.com/title/" + tv_id + "'>"
-	body += "<img alt='" + series + " Poster' src='" + link + "' height=192px></a></div>"
-	body += "<div style='width:60%; height:200px; float:left;'><br><br>"
-	body += "<b>TV Series name: </b>" + series + "<br><br>"
-	body += "<b>IMDb Rating: </b>" + rating + "<br><br>"
-	body += "<b>Status: </b>" + showStatus(temp) + "</div><hr>"	
-
-if body=='':
-	exit()
-else:
-	body = body[0:-4]
-
-try:
-	msg = MIMEMultipart()
-	msg['From'] = sender
-	msg['To'] = destination
-	msg['Subject'] = subject
-	msg.attach(MIMEText(body, 'html'))
-
-	conn = SMTP_SSL(SMTPserver)
-	conn.set_debuglevel(False)
-	conn.login(sender, password)
-
-	try:
-		conn.sendmail(sender, [destination], msg.as_string())
-		print "Email sent to " + destination + " successfully!"
-	finally:
-		conn.quit()
-
-except:
-	print "Incorrect email! PLease type in a correct email."
+insertIntoDB(email, tv_series)
+body = getStatus(tv_series)
+sendEmail(email, body)
